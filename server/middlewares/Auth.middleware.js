a6fbda2de38911737d101e9d17234f3361a4a842
MiguelRicardo2003 <miguelricardomm27@gmail.com>
2025-10-03 05:52:52 -0500

    ajustes en el deploy

--- server/middlewares/Auth.middleware.js
+++ server/middlewares/Auth.middleware.js
@@ -1,20 +1,22 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export default function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ message: 'No autorizado. Falta token.' });
+    return res.status(401).json({ message: "No autorizado. Falta token." });
   }
 
-  // JWT real
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'dev_secret_change_me');
+    // Usa el JWT_SECRET de Supabase
+    const decoded = jwt.verify(
+      token,
+      process.env.SUPABASE_JWT_SECRET 
+    );
     req.usuario = decoded;
     return next();
   } catch (error) {
-    return res.status(401).json({ message: 'Token inválido' });
+    return res.status(401).json({ message: "Token inválido" });
   }
 }
-  
